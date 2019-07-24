cc56cb0f8c803c1d88706b78e97e2aca1c7d62e2
sreifens <sreifens@ucsc.edu>
2019-07-24 12:14:28 -0700

    Added toString method to Phone.java

--- src/main/java/com/genesys/provisioning/models/Phone.java
+++ src/main/java/com/genesys/provisioning/models/Phone.java
@@ -81,5 +81,14 @@ public class Phone {
     	this.placeName = placeName;
     	return this;
     }
+    
+    @Override
+	public String toString() {
+		return "{"
+			+ "number = " + this.number+" , "
+			+ "description = " + this.description+" , "
+			+ "isDefault = " + this.isDefault+" , "
+			+ "placeName = " + this.placeName+"}";
+	}
 
 }
